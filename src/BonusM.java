34842b1452df5f1d631bb9a92847a5d19a90c2f4
Ksenia Starchenko <starchenko.ksenia@gmail.com>
2023-02-20 17:12:51 +0400

    Update Main.java

---
+++ src/BonusM.java
@@ -0,0 +1,16 @@
+public class BonusM {
+
+    public int calculate (int cost){
+        int result;
+        if (cost >= 20) {
+            result = cost / 20;
+        } else {
+            result = 0;
+        }
+        return result;
+
+        }
+
+
+}
+
