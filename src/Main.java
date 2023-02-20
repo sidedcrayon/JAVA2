1ba3e72fa4ae03e2be0b994975e7e39eb5a31cfc
sidedcrayon <123884901+sidedcrayon@users.noreply.github.com>
2023-02-20 16:09:17 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int price = 20000;
+        int bonus = 20;
+        int milli = price / bonus;
+
+        System.out.print (milli);
+     
     }
 }
